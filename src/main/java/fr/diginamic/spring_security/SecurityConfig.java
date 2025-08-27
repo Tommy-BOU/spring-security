de50aa3e5dc721df455214144f85fa674127732d
Tommy-BOU <tommy.boubaker@free.fr>
2025-08-27 14:13:49 +0200

    tp 1b

--- src/main/java/fr/diginamic/spring_security/SecurityConfig.java
+++ src/main/java/fr/diginamic/spring_security/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +18,43 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(Customizer.withDefaults())
+
+                .csrf(csrf -> csrf.disable())
+//                .headers(headers -> headers
+//                        .frameOptions(frameOptions -> frameOptions
+//                        .sameOrigin()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/hello").permitAll()
-                        .requestMatchers("/hello-private").authenticated())
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/hello-private", true)
+//                        .failureUrl("/hello")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/hello")
+                        .permitAll())
                 .build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user1 = User.withUsername("user1")
+//                .password("user1")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password("admin")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, admin);
+//    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
